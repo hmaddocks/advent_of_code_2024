0ba06d985d314c23d47ccd6815dfdb76f62a372c
Henry Maddocks <hmaddocks@me.com>
2024-12-12 14:44:40 +1300

    Remove string conversions

--- elixir/day11/lib/part1.ex
+++ elixir/day11/lib/part1.ex
@@ -10,22 +10,18 @@ defmodule Part1 do
   def process_stone(0, blinks), do: process_stone(1, blinks - 1)
 
   def process_stone(stone, blinks) do
-    digits = Integer.to_string(abs(stone))
-    process_stone_digits(stone, digits, blinks)
-  end
+    stone = abs(stone)
+    num_digits = trunc(:math.log10(stone)) + 1
 
-  defp process_stone_digits(stone, digits, blinks) do
-    case rem(String.length(digits), 2) do
-      0 ->
-        mid = div(String.length(digits), 2)
-        {first_half, second_half} = String.split_at(digits, mid)
+    if rem(num_digits, 2) == 0 do
+      power = trunc(:math.pow(10, div(num_digits, 2)))
+      first_half = div(stone, power)
+      second_half = rem(stone, power)
 
-        [first_half, second_half]
-        |> Enum.map(&String.to_integer/1)
-        |> Enum.flat_map(&process_stone(&1, blinks - 1))
-
-      _ ->
-        process_stone(stone * 2024, blinks - 1)
+      [first_half, second_half]
+      |> Enum.flat_map(&process_stone(&1, blinks - 1))
+    else
+      process_stone(stone * 2024, blinks - 1)
     end
   end
 
